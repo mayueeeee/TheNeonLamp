048b8f6d868b037973429bee205dd233aeb1a003
Sarunyu Chankong <sarunyu.you@gmail.com>
2018-04-08 02:54:11 +0700

    Fix size

--- Web panel/src/Components/BackgroundWarpper.js
+++ Web panel/src/Components/BackgroundWarpper.js
@@ -2,6 +2,6 @@ import styled from 'styled-components'
 const bg = styled.div`
   background-color: ${props => props.color};
   ${props => props.full ? 'width: 100vw' : ''}
-  ${props => props.full ? 'height: 100vh' : ''}
+  ${props => props.full ? 'height: 120vh' : ''}
 `
 export default bg
